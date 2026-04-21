7a909926c2bcdbac42828f8358aebe96dbb131d0
Novikova Alexandra <novikova.s2007@mail.ru>
2026-04-21 18:45:32 +0300

    Библиотеки, константы, Init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#ПРОВЕРКА
+import os
+import sys
+import shutil
+
+MYGIT_DIR = ".mygit"
+COMMITS_DIR = os.path.join(MYGIT_DIR, "commits")
+
+
+def init():
+    if os.path.exists(MYGIT_DIR):
+        print("Repository already initialized")
+        return
+
+    os.makedirs(COMMITS_DIR)
+    print("Initialized empty mygit repository")
