325018c1639f7a9a5bde813e8074e0944ce75d8d
Novikova Alexandra <novikova.s2007@mail.ru>
2026-04-23 17:17:07 +0300

    ignored_files should_ignore init

--- main.py
+++ main.py
@@ -2,8 +2,33 @@ import os
 import sys
 import shutil
 
+
 MYGIT_DIR = ".mygit"
 COMMITS_DIR = os.path.join(MYGIT_DIR, "commits")
+MESSAGE_DIR = os.path.join(MYGIT_DIR, ".message")
+SEQUENCE_FILE = os.path.join(MYGIT_DIR, ".sequence")
+IGNORE_FILE = ".mygitignore"
+
+
+def get_ignored_files():
+    ignored = set()
+
+    if os.path.exists(IGNORE_FILE):
+        with open(IGNORE_FILE, "r") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    ignored.add(line)
+
+    ignored.add(MYGIT_DIR)
+    return ignored
+
+
+def should_ignore(path, ignored):
+    for item in ignored:
+        if path.startswith(item):
+            return True
+    return False
 
 
 def init():
@@ -12,4 +37,9 @@ def init():
         return
 
     os.makedirs(COMMITS_DIR)
+    os.makedirs(MESSAGE_DIR)
+
+    with open(SEQUENCE_FILE, "w") as f:
+        f.write("1")
+
     print("Initialized empty mygit repository")
